805f2b5f681dbdd27add2ded998a5688c4a4090e
Fatma Nur Kurt <kurtfatmanur8@gmail.com>
2024-07-01 14:56:31 +0300

    Set up project structure and basic file handling Established basic MVC architecture. Implemented file upload and retrieval functionalities. Created essential entities and repository for movie management.

---
+++ src/main/java/com/movieflix/movieApi/dto/MovieDto.java
@@ -0,0 +1,30 @@
+package com.movieflix.movieApi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDto {
+
+
+    private Integer movieId;
+    @NotBlank(message = "Please provide movie's title!")
+    private String title;
+    @NotBlank(message = "Please provide movie's director!")
+    private String director;
+    @NotBlank(message = "Please provide movie's studio!")
+    private String studio;
+
+    private Set<String> movieCast;
+    private Integer releaseYear;
+    @NotBlank(message = "Please provide movie's release poster!")
+    private String poster;
+    @NotBlank(message = "Please provide movie's release posterUrl!")
+    private String posterUrl;
+}
